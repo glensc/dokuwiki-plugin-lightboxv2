b46bf5a7f0a3c4dfb1cdd7f6daba3ff7c0a1c08b
Elan Ruusamäe <glen@delfi.ee>
2012-10-06 19:12:49 +0300

    return early

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ jQuery(function($) {
 
 	$('a[class=media]').each(function() {
 		var img = $(this).find('img');
-		if (img.length) {
-			img = img[0];
-			var url = img.src;
-
-			// remove size specification
-			// userewrite=0: /lib/exe/fetch.php?w=200&media=image.png, fetch.php?media=image.png
-			// userewrite=1: /_media/image.png?w=200, /_media/image.png
-			// userewrite=2: /lib/exe/fetch.php/image.png?w=200, /lib/exe/fetch.php/image.png
-			url = url.replace(/\?w=\d+&?/, '?');
-			jsonData.push([url, img.title || '']);
-
-			// rewrite .href, so lightbox could find active image from set
-			// this is perhaps evil, but then again, we overtake click handler
-			// anyway.
-			this.href = url;
-		} else {
+		if (!img.length) {
 			return false;
 		}
+
+		img = img[0];
+		var url = img.src;
+
+		// remove size specification
+		// userewrite=0: /lib/exe/fetch.php?w=200&media=image.png, fetch.php?media=image.png
+		// userewrite=1: /_media/image.png?w=200, /_media/image.png
+		// userewrite=2: /lib/exe/fetch.php/image.png?w=200, /lib/exe/fetch.php/image.png
+		url = url.replace(/\?w=\d+&?/, '?');
+		jsonData.push([url, img.title || '']);
+
+		// rewrite .href, so lightbox could find active image from set
+		// this is perhaps evil, but then again, we overtake click handler
+		// anyway.
+		this.href = url;
 	}).lightbox({
 		fileLoadingImage: DOKU_BASE +'lib/plugins/lightbox/jquery-lightbox/images/loading.gif',
 		fileBottomNavCloseImage: DOKU_BASE +'lib/plugins/lightbox/jquery-lightbox/images/closelabel.gif',
